b2e49b12141b5c7fc726edc786e73643cafe4295
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2023-03-04 13:48:07 +0400

    1.6.0

--- Sources/SwiftOpenAPI/Objects/OAuthFlowObject.swift
+++ Sources/SwiftOpenAPI/Objects/OAuthFlowObject.swift
@@ -3,19 +3,19 @@ import Foundation
 public struct OAuthFlowObject: Codable, Equatable, SpecificationExtendable {
     
     /// The authorization URL to be used for this flow. This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
-    public var authorizationUrl: URL
+    public var authorizationUrl: String?
     
     /// The token URL to be used for this flow. This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
-    public var tokenUrl: URL?
+    public var tokenUrl: String?
     
     /// The URL to be used for obtaining refresh tokens. This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
-    public var refreshUrl: URL?
+    public var refreshUrl: String?
     
     /// The available scopes for the OAuth2 security scheme. A map between the scope name and a short description for it. The map MAY be empty.
     public var scopes: [String: String]?
     
     
-    public init(authorizationUrl: URL, tokenUrl: URL? = nil, refreshUrl: URL? = nil, scopes: [String: String]? = nil) {
+    public init(authorizationUrl: String? = nil, tokenUrl: String? = nil, refreshUrl: String? = nil, scopes: [String: String]? = nil) {
         self.authorizationUrl = authorizationUrl
         self.tokenUrl = tokenUrl
         self.refreshUrl = refreshUrl
