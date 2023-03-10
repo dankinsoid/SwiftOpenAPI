bb2d7dd023944fc4fa4822508e6ab2cd61932035
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2023-03-11 02:13:58 +0400

    1.14.0

--- Sources/SwiftOpenAPI/OpenAPIPrimitive.swift
+++ Sources/SwiftOpenAPI/OpenAPIPrimitive.swift
@@ -27,67 +27,67 @@ extension StaticString: OpenAPIType {
 
 extension Int: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "int64") }
 }
 
 extension Int8: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "int8") }
 }
 
 extension Int16: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "int16") }
 }
 
 extension Int32: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "int32") }
 }
 
 extension Int64: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "int64") }
 }
 
 extension UInt: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "uint64") }
 }
 
 extension UInt8: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "uint8") }
 }
 
 extension UInt16: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "uint16") }
 }
 
 extension UInt32: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "uint32") }
 }
 
 extension UInt64: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.integer) }
+    public static var openAPISchema: SchemaObject { .primitive(.integer, format: "uint64") }
 }
 
 extension Double: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.number) }
+    public static var openAPISchema: SchemaObject { .primitive(.number, format: "double") }
 }
 
 extension Float: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.number) }
+    public static var openAPISchema: SchemaObject { .primitive(.number, format: "float") }
 }
 
 extension Decimal: OpenAPIType {
     
-    public static var openAPISchema: SchemaObject { .primitive(.number) }
+    public static var openAPISchema: SchemaObject { .primitive(.number, format: "decimal") }
 }
 
 extension Date: OpenAPIType {
